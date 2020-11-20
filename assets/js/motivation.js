0ba88fbbad36a56f6dff7ef7a726799710d25821
julienfouquet <hello@julienfouquet.fr>
2020-11-20 10:17:12 +0100

    anonymous letter

--- assets/js/motivation.js
+++ assets/js/motivation.js
@@ -1,70 +1,24 @@
 let str = [{
   key: '(emploi)',
-  value: 'Développeur front-end confirmé (H/F)'
+  value: 'JOB'
 }, {
   key: '(reference)',
-  value: '&middot; Ref TLSDEVFRONT'
+  value: '&middot; REF'
 }, {
   key: '(competences)',
-  value: 'développement, référent technique, mises en place de bonnes pratiques...'
+  value: 'SKILLS'
 }, {
   key: '(company)',
-  value: 'ACTIVUS'
+  value: 'COMPANY'
 }, {
   key: '(adress)',
-  value: 'Bat E  &middot; 1 chemin du Pigeonnier de la Cépière'
+  value: 'ADDRESS'
 }, {
   key: '(adress2)',
-  value: '31100 TOULOUSE'
+  value: 'ZIP CITY'
 }, {
   key: '(when)',
-  value: 'Fait à Tournefeuille, le 06/05/2020'
-}
-]
-str = [{
-  key: '(emploi)',
-  value: 'Développeur front-end Senior (H/F)'
-}, {
-  key: '(reference)',
-  value: '&middot; Ref IT01331'
-}, {
-  key: '(competences)',
-  value: 'développement, référent technique, mises en place de bonnes pratiques...'
-}, {
-  key: '(company)',
-  value: 'TAlENTS-RH'
-}, {
-  key: '(adress)',
-  value: 'TOUOUSE'
-}, {
-  key: '(adress2)',
-  value: ''
-}, {
-  key: '(when)',
-  value: 'Fait à Tournefeuille, le 06/05/2020'
-}
-]
-str = [{
-  key: '(emploi)',
-  value: 'Expert Front-End'
-}, {
-  key: '(reference)',
-  value: ''
-}, {
-  key: '(competences)',
-  value: 'développement, référent technique, mises en place de bonnes pratiques, veille technologique, évolution...'
-}, {
-  key: '(company)',
-  value: 'Monkey Patch'
-}, {
-  key: '(adress)',
-  value: 'TOUOUSE'
-}, {
-  key: '(adress2)',
-  value: ''
-}, {
-  key: '(when)',
-  value: 'Fait à Tournefeuille, le 29/06/2020'
+  value: 'DO WHEN'
 }
 ]
 
