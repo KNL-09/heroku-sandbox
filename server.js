c96d6b71d2105945a06a7ba6b09c48ca996a4579
KNL-09 <73900681+KNL-09@users.noreply.github.com>
2023-09-16 16:51:42 +0200

    rm unused files

--- server.js
+++ server.js
@@ -3,31 +3,15 @@ var bodyParser = require('body-parser');
 const port = process.env.PORT || 4800;
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
-
-app.get('/messages', (req, res) => {
-   // Si on a un historique stocké quelque part
-})
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Rewrite without extension
 app.use(express.static(__dirname + '/', {
   extensions: ['html']
 }));
-
-io.on('connection', (socket) =>{
-  console.log('a user is connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-  socket.on('message', (msg) => {
-    console.log('message : ' + msg);
-    io.emit('message', msg);
-  });
-})
 
 // Listen to the port
 http.listen(port, (err) => {
